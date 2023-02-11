6ca1e10c7a2a534c6aafe5c7ba68f12323ac020a
Fikri Rasyid <fikrirasyid@gmail.com>
2023-02-11 10:28:37 +0700

    Fixed formatting using phpcbf

--- d5i-modal-dev-state-monitor.php
+++ d5i-modal-dev-state-monitor.php
@@ -29,28 +29,31 @@ along with D5 Modal Dev State Monitor. If not, see https://www.gnu.org/licenses/
  * Add custom item on admin bar for `State Monitor`
  *
  * @since ??
- *
  */
 function d5_state_monitor_admin_bar_link( $admin_bar ) {
 
 	// Only display this admin bar item on D5 Visual Builder.
 	if ( function_exists( 'et_builder_d5_enabled' ) && et_builder_d5_enabled() && et_core_is_fb_enabled() ) {
-        $d5_dev_tools_id = 'd5-dev-tools';
+		$d5_dev_tools_id = 'd5-dev-tools';
 
-        // Main menu.
-        $admin_bar->add_node( array(
-            'id'    => $d5_dev_tools_id,
-            'title' => __( 'D5 Dev Tools', 'd5-modal-dev-state-monitor' ),
-            'href'  => '#',
-        ) );
+		// Main menu.
+		$admin_bar->add_node(
+			array(
+				'id'    => $d5_dev_tools_id,
+				'title' => __( 'D5 Dev Tools', 'd5-modal-dev-state-monitor' ),
+				'href'  => '#',
+			)
+		);
 
-        // Sub Menu.
-        $admin_bar->add_node( array(
-            'parent' => $d5_dev_tools_id,
-            'id'     => 'd5-modal-dev-state-monitor',
-            'title'  => 'State Monitor',
-            'href'   => '#d5-state-monitor'
-        ) );
+		// Sub Menu.
+		$admin_bar->add_node(
+			array(
+				'parent' => $d5_dev_tools_id,
+				'id'     => 'd5-modal-dev-state-monitor',
+				'title'  => 'State Monitor',
+				'href'   => '#d5-state-monitor',
+			)
+		);
 	}
 }
 add_action( 'admin_bar_menu', 'd5_state_monitor_admin_bar_link', 700 );
@@ -61,29 +64,29 @@ add_action( 'admin_bar_menu', 'd5_state_monitor_admin_bar_link', 700 );
  * @since ??
  */
 function d5_state_monitor_enqueue_scripts() {
-    if ( function_exists( 'et_builder_d5_enabled' ) && et_builder_d5_enabled() && et_core_is_fb_enabled() ) {
-        $plugin_dir_url = plugin_dir_url( __FILE__ );
+	if ( function_exists( 'et_builder_d5_enabled' ) && et_builder_d5_enabled() && et_core_is_fb_enabled() ) {
+		$plugin_dir_url = plugin_dir_url( __FILE__ );
 
-        wp_enqueue_script(
-            "d5-state-monitor-builder-bundle-script",
-            "{$plugin_dir_url}scripts/bundle.js",
-            array(
-                'divi-visual-builder',
-                'divi-data',
-                'divi-error-boundary',
-                'divi-modal',
-                'divi-object-renderer'
-            ),
-            '0.1.0',
-            true
-        );
+		wp_enqueue_script(
+			'd5-state-monitor-builder-bundle-script',
+			"{$plugin_dir_url}scripts/bundle.js",
+			array(
+				'divi-visual-builder',
+				'divi-data',
+				'divi-error-boundary',
+				'divi-modal',
+				'divi-object-renderer',
+			),
+			'0.1.0',
+			true
+		);
 
-        wp_enqueue_style(
-            "d5-state-monitor-builder-bundle-style",
-            "{$plugin_dir_url}styles/bundle.css",
-            array(),
-            '0.1.0'
-        );
-    }
+		wp_enqueue_style(
+			'd5-state-monitor-builder-bundle-style',
+			"{$plugin_dir_url}styles/bundle.css",
+			array(),
+			'0.1.0'
+		);
+	}
 }
 add_action( 'et_vb_assets_after_enqueue_package_script', 'd5_state_monitor_enqueue_scripts' );
