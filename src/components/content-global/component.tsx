a70c5dc9a1e987988f4489048973d2b755fa6f19
Fikri Rasyid <fikrirasyid@gmail.com>
2023-08-31 12:01:10 +0700

    Added no global data found notice when there is no global data

--- src/components/content-global/component.tsx
+++ src/components/content-global/component.tsx
@@ -1,6 +1,9 @@
 // External dependencies.
 import React, { ReactElement } from 'react';
 
+// Divi dependencies.
+import { __ } from '@wordpress/i18n';
+
 // Local dependencies.
 import { ModuleTreeView } from '../module-tree-view';
 import { ContentGlobalProps } from './types';
@@ -20,25 +23,36 @@ export const ContentGlobal = ({
   selectedModules,
   setExpandedModuleIds,
   globalModules,
-}: ContentGlobalProps): ReactElement[] => (
-  globalModules.map(globalModule => (
-    <div
-      key={`global-module-item-${globalModule?.id}`}
-      className="d5-dev-tool-global-module-item"
-    >
-      <h3>id: {globalModule?.id}</h3>
-      <ModuleTreeView
-        activeModalSetting={activeModalSetting}
-        draggedModules={draggedModules}
-        expandedModuleIds={expandedModuleIds}
-        hoveredModule={hoveredModule}
-        lastModuleClipboard={lastModuleClipboard}
-        modules={modules}
-        root={globalModule?.content?.root}
-        rightClickedModuleId={rightClickedModuleId}
-        selectedModules={selectedModules}
-        setExpandedModuleIds={setExpandedModuleIds}
-      />
-    </div>
-  ))
-);
+}: ContentGlobalProps): ReactElement | ReactElement[] => {
+
+  if (!globalModules?.length) {
+    return (
+      <div className="d5-dev-tool-no-items">
+        {__('No global data found', 'divi-5-dev-tool')}
+      </div>
+    );
+  }
+
+  return (
+    globalModules.map(globalModule => (
+      <div
+        key={`global-module-item-${globalModule?.id}`}
+        className="d5-dev-tool-global-module-item"
+      >
+        <h3>id: {globalModule?.id}</h3>
+        <ModuleTreeView
+          activeModalSetting={activeModalSetting}
+          draggedModules={draggedModules}
+          expandedModuleIds={expandedModuleIds}
+          hoveredModule={hoveredModule}
+          lastModuleClipboard={lastModuleClipboard}
+          modules={modules}
+          root={globalModule?.content?.root}
+          rightClickedModuleId={rightClickedModuleId}
+          selectedModules={selectedModules}
+          setExpandedModuleIds={setExpandedModuleIds}
+        />
+      </div>
+    ))
+  )
+};
